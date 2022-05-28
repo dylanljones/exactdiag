5c9d6034a4439da5d68728d78360ce0543d0c36e
dylanljones <dylanljones94@gmail.com>
2022-05-28 23:47:35 +0200

    feat: add config file support

--- exactdiag/utils.py
+++ exactdiag/utils.py
@@ -4,8 +4,10 @@
 #
 # Copyright (c) 2022, Dylan Jones
 
+import os
+import tomli
+import numba
 import logging
-
 
 logger = logging.getLogger(__name__.split(".")[0])
 
@@ -22,3 +24,38 @@ logger.addHandler(sh)
 # Set logging level
 logger.setLevel(logging.WARNING)
 logging.root.setLevel(logging.NOTSET)
+
+
+def _read_config():
+    files = ["ed.toml", "exactdiag.toml", "pyproject.toml"]
+    for file in files:
+        try:
+            with open(file, "rb") as fh:
+                data = tomli.load(fh)
+            return dict(data["exactdiag"])
+        except (FileNotFoundError, KeyError):
+            pass
+    return dict()
+
+
+def parse_config():
+
+    data = _read_config()
+    num_threads = data.get("numba_threads", -1)
+    if num_threads <= 0:
+        num_threads = os.cpu_count() + num_threads
+
+    cache_dir = data.get("cache_dir", "")
+    if not cache_dir:
+        cache_dir = "__edcache__"
+
+    return {
+        "numba_threads": num_threads,
+        "cache_dir": cache_dir,
+    }
+
+
+CONFIG = parse_config()
+CACHE_DIR = CONFIG["cache_dir"]
+
+numba.set_num_threads(CONFIG["numba_threads"])
