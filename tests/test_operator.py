ec8b4259c22b158e25257444cc0ade029f2eddf7
dylanljones <dylanljones94@gmail.com>
2022-06-01 22:09:28 +0200

    test: test creation/annihilation operators using a naive implementation

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -29,6 +29,70 @@ def test_project_dn(dn_idx):
     assert_array_equal(indices, result)
 
 
+def _build_creation_naive(sector, sector_p1, pos, sigma):
+    arr = np.zeros((sector_p1.size, sector.size))
+    op = 1 << pos
+    for i, state_i in enumerate(sector.states):
+        for j, state_j in enumerate(sector_p1.states):
+            if sigma == ed.UP:
+                numi_s, numj_s = state_i.up, state_j.up
+                numi_stilde, numj_stilde = state_i.dn, state_j.dn
+            else:
+                numi_s, numj_s = state_i.dn, state_j.dn
+                numi_stilde, numj_stilde = state_i.up, state_j.up
+
+            if not (numi_s & op):
+                if (numi_s ^ op) == numj_s and (numi_stilde == numj_stilde):
+                    arr[j, i] = 1
+    return arr
+
+
+def _build_annihilation_naive(sector, sector_p1, pos, sigma):
+    arr = np.zeros((sector_p1.size, sector.size))
+    op = 1 << pos
+    for i, state_i in enumerate(sector.states):
+        for j, state_j in enumerate(sector_p1.states):
+            if sigma == ed.UP:
+                numi_s, numj_s = state_i.up, state_j.up
+                numi_stilde, numj_stilde = state_i.dn, state_j.dn
+            else:
+                numi_s, numj_s = state_i.dn, state_j.dn
+                numi_stilde, numj_stilde = state_i.up, state_j.up
+
+            if numj_s & op:
+                if (numj_s ^ op) == numi_s and (numi_stilde == numj_stilde):
+                    arr[j, i] = 1
+    return arr
+
+
+@mark.parametrize("num_sites", [2, 3, 4, 5])
+@mark.parametrize("sigma", [ed.UP, ed.DN])
+def test_creation_operator(num_sites, sigma):
+    basis = ed.Basis(num_sites)
+    for n_up, n_dn in basis.iter_fillings():
+        sector = basis.get_sector(n_up, n_dn)
+        sector_p1 = basis.upper_sector(n_up, n_dn, sigma)
+        if sector_p1 is not None:
+            for pos in range(num_sites):
+                cop_dag = operators.CreationOperator(sector, sector_p1, pos, sigma)
+                expected = _build_creation_naive(sector, sector_p1, pos, sigma)
+                assert_array_equal(expected, cop_dag.toarray())
+
+
+@mark.parametrize("num_sites", [2, 3, 4, 5])
+@mark.parametrize("sigma", [ed.UP, ed.DN])
+def test_annihilation_operator(num_sites, sigma):
+    basis = ed.Basis(num_sites)
+    for n_up, n_dn in basis.iter_fillings():
+        sector = basis.get_sector(n_up, n_dn)
+        sector_p1 = basis.upper_sector(n_up, n_dn, sigma)
+        if sector_p1 is not None:
+            for pos in range(num_sites):
+                cop_dag = operators.CreationOperator(sector, sector_p1, pos, sigma)
+                expected = _build_annihilation_naive(sector, sector_p1, pos, sigma)
+                assert_array_equal(expected, cop_dag.toarray())
+
+
 @mark.parametrize("num_sites", [2, 3, 4, 5])
 @mark.parametrize("sigma", [ed.UP, ed.DN])
 def test_creation_annihilation_adjoint(num_sites, sigma):
