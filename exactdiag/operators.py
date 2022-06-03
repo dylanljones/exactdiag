fix: fix sign of creation/annihilation operators (fixes #1)

exactdiag/operators.py
@@ -670,9 +670,9 @@ def _apply_creation_up(matvec, x, up_states, dn_states, up_states_p1, pos):
     num_dn = len(dn_states)
     all_dn = np.arange(num_dn)
 
-    counts = np.zeros((num_dn, 1))
-    for i, dn in enumerate(dn_states):
-        counts[i] = bit_count_between(dn, 0, pos)
+    # counts = np.zeros((num_dn, 1))
+    # for i, dn in enumerate(dn_states):
+    #     counts[i] = bit_count_between(dn, 0, pos)
 
     for up_idx, up in enumerate(up_states):
         if not (up & op):
@@ -680,12 +680,12 @@ def _apply_creation_up(matvec, x, up_states, dn_states, up_states_p1, pos):
             idx_new = bisect_left(up_states_p1, new)
             origins = up_idx * num_dn + all_dn
             targets = idx_new * num_dn + all_dn
-            signs = (-1) ** (counts + bit_count_between(up, 0, pos))
+            signs = (-1) ** bit_count_between(up, 0, pos)
             matvec[targets] = signs * x[origins]
 
 
 @njit(**_jitkw)
-def _apply_creation_dn(matvec, x, up_states, dn_states, dn_states_p1, pos):
+def _apply_creation_dn(matvec, x, num_sites, up_states, dn_states, dn_states_p1, pos):
     op = 1 << pos
     num_dn = len(dn_states)
     num_up = len(up_states)
@@ -694,7 +694,7 @@ def _apply_creation_dn(matvec, x, up_states, dn_states, dn_states_p1, pos):
 
     counts = np.zeros((num_up, 1))
     for i, up in enumerate(up_states):
-        counts[i] = bit_count_between(up, 0, pos)
+        counts[i] = bit_count(up, num_sites)
 
     for dn_idx, dn in enumerate(dn_states):
         if not (dn & op):
@@ -707,14 +707,10 @@ def _apply_creation_dn(matvec, x, up_states, dn_states, dn_states_p1, pos):
 
 
 @njit(**_jitkw)
-def _apply_annihilation_up(matvec, x, up_states, dn_states, up_states_p1, pos):
+def _apply_annihilation_up(matvec, x, up_states, dn_states_p1, up_states_p1, pos):
     op = 1 << pos
-    num_dn = len(dn_states)
+    num_dn = len(dn_states_p1)
     all_dn = np.arange(num_dn)
-
-    counts = np.zeros((num_dn, 1))
-    for i, dn in enumerate(dn_states):
-        counts[i] = bit_count_between(dn, 0, pos)
 
     for up_idx, up in enumerate(up_states_p1):
         if up & op:
@@ -722,21 +718,23 @@ def _apply_annihilation_up(matvec, x, up_states, dn_states, up_states_p1, pos):
             idx_new = bisect_left(up_states, new)
             origins = up_idx * num_dn + all_dn
             targets = idx_new * num_dn + all_dn
-            signs = (-1) ** (counts + bit_count_between(up, 0, pos))
+            signs = (-1) ** bit_count_between(up, 0, pos)
             matvec[targets] = signs * x[origins]
 
 
 @njit(**_jitkw)
-def _apply_annihilation_dn(matvec, x, up_states, dn_states, dn_states_p1, pos):
+def _apply_annihilation_dn(
+    matvec, x, num_sites, up_states_p1, dn_states, dn_states_p1, pos
+):
     op = 1 << pos
-    num_up = len(up_states)
+    num_up = len(up_states_p1)
     num_dn = len(dn_states_p1)
     all_up = np.arange(num_up) * num_dn
     all_up_p1 = np.arange(num_up) * len(dn_states)
 
     counts = np.zeros((num_up, 1))
-    for i, up in enumerate(up_states):
-        counts[i] = bit_count_between(up, 0, pos)
+    for i, up in enumerate(up_states_p1):
+        counts[i] = bit_count(up, num_sites)
 
     for dn_idx, dn in enumerate(dn_states_p1):
         if dn & op:
@@ -775,10 +773,13 @@ class CreationOperator(LinearOperator):
         _apply_creation_up(matvec, x, up_states, dn_states, up_states_p1, self.pos)
 
     def _build_dn(self, matvec, x):
+        nsites = self.sector.num_sites
         up_states = self.sector.up_states
         dn_states = self.sector.dn_states
         dn_states_p1 = self.sector_p1.dn_states
-        _apply_creation_dn(matvec, x, up_states, dn_states, dn_states_p1, self.pos)
+        _apply_creation_dn(
+            matvec, x, nsites, up_states, dn_states, dn_states_p1, self.pos
+        )
 
     def _matvec(self, x):
         matvec = np.zeros((self.shape[0], *x.shape[1:]), dtype=x.dtype)
@@ -821,11 +822,12 @@ class AnnihilationOperator(LinearOperator):
         )
 
     def _build_dn(self, matvec, x):
+        num_sites = self.sector.num_sites
         dn_states = self.sector_m1.dn_states
         up_states_p1 = self.sector.up_states
         dn_states_p1 = self.sector.dn_states
         _apply_annihilation_dn(
-            matvec, x, up_states_p1, dn_states, dn_states_p1, self.pos
+            matvec, x, num_sites, up_states_p1, dn_states, dn_states_p1, self.pos
         )
 
     def _matvec(self, x):

tests/test_greens.py
@@ -28,7 +28,6 @@ def test_gf_lehmann_hubbard_non_interacting(num_sites):
     Green's function with the same on-site and hopping energy.
     """
     eps = 0.0
-    pos = 0
     beta = 100  # Low temp to match non-interacting GF
     z = np.linspace(-6, +6, 1001) + 0.05j
 
@@ -37,11 +36,10 @@ def test_gf_lehmann_hubbard_non_interacting(num_sites):
 
     ham0 = tight_binding_hamiltonian(latt, eps=model.eps, hop=model.hop)
     gf0_z_diag = gf0_pole(ham0, z=z)
-
-    gf0_z = gf0_z_diag[:, pos]
-    gf_z = gf_lehmann(model, z, i=pos, j=pos, occ=False)[0]
-
-    assert_allclose(gf_z, gf0_z, rtol=1e-4, atol=1e-3)
+    for pos in range(num_sites):
+        gf0_z = gf0_z_diag[:, pos]
+        gf_z = gf_lehmann(model, z, i=pos, j=pos, occ=False)[0]
+        assert_allclose(gf_z, gf0_z, rtol=1e-4, atol=1e-3)
 
 
 @mark.parametrize("num_sites", [2, 3, 4, 5])

tests/test_operator.py
@@ -47,27 +47,28 @@ def test_count_bits_between(num):
             assert operators.bit_count_between(num, start, stop) == expected
 
 
+def op_sign(up, dn, i, sigma, num_sites):
+    if sigma == ed.UP:
+        count = ed.operators.bit_count_between(up, 0, i)
+    else:
+        count = ed.operators.bit_count_between(up, 0, num_sites)
+        count += ed.operators.bit_count_between(dn, 0, i)
+    return (-1) ** count
+
+
 def _build_creation_naive(sector, sector_p1, pos, sigma):
     arr = np.zeros((sector_p1.size, sector.size))
     op = 1 << pos
     for i, state_i in enumerate(sector.states):
         for j, state_j in enumerate(sector_p1.states):
-            if sigma == ed.UP:
-                numi_s, numj_s = state_i.up, state_j.up
-                numi_stilde, numj_stilde = state_i.dn, state_j.dn
-            else:
-                numi_s, numj_s = state_i.dn, state_j.dn
-                numi_stilde, numj_stilde = state_i.up, state_j.up
-
-            if not (numi_s & op):
-                if (numi_s ^ op) == numj_s and (numi_stilde == numj_stilde):
-                    # Count bits set between 0 and pos-1 for both spin states
-                    count_s = ed.operators.bit_count_between(numi_s, 0, pos)
-                    count_stilde = ed.operators.bit_count_between(numi_stilde, 0, pos)
-                    # Count is sum of both spin states
-                    count = count_s + count_stilde
-                    sign = (-1) ** count
-                    arr[j, i] = sign
+            up_i, up_j = state_i.up, state_j.up
+            dn_i, dn_j = state_i.dn, state_j.dn
+            if sigma == ed.UP and not (up_i & op):
+                if (up_i ^ op) == up_j and (dn_i == dn_j):
+                    arr[j, i] = op_sign(up_i, dn_i, pos, ed.UP, sector.num_sites)
+            elif sigma == ed.DN and not (dn_i & op):
+                if (dn_i ^ op) == dn_j and (up_i == up_j):
+                    arr[j, i] = op_sign(up_i, dn_i, pos, ed.DN, sector.num_sites)
     return arr
 
 